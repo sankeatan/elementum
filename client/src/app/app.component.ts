755b67217f94ba69e4cc3b9c6a1bd5a81ab72ff1
David Ferguson <deejai@live.com>
2022-07-10 13:46:11 -0600

    used ngfor in dropdown

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import io from "socket.io-client";
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core'
+import { io } from "socket.io-client"
 import { elements, drawBoard } from "./display/elements"
 
 @Component({
@@ -10,29 +10,42 @@ import { elements, drawBoard } from "./display/elements"
 export class AppComponent implements OnInit, AfterViewInit {
 
   @ViewChild("game")
-  private gameCanvas!: ElementRef;
+  private gameCanvas!: ElementRef
 
-  private context: any;
-  private socket: any;
+  private context: any
+  private socket: any
 
   public ngOnInit(): void {
-    this.socket = io("http://localhost:3000", { transports : ['websocket'] });
+    this.socket = io("http://localhost:3000", { transports : ['websocket'] })
+  }
+
+  private getCursorPosition(canvas: HTMLCanvasElement, event: MouseEvent) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    console.log("x: " + x + " y: " + y)
   }
 
   public ngAfterViewInit(): void {
-    this.context = this.gameCanvas.nativeElement.getContext("2d");
-    this.socket.on("position", (position: { x: number; y: number; }): void => {
-      console.log(position);
-      // this.context.clearRect(0, 0, this.gameCanvas.nativeElement.width, this.gameCanvas.nativeElement.height);
-      this.context.fillStyle = "black";
-      this.context.fillRect(position.x, position.y, 20, 20);
+    this.context = this.gameCanvas.nativeElement.getContext("2d")
+    this.socket.on("position", (position: { x: number; y: number }): void => {
+      console.log(position)
+      // this.context.clearRect(0, 0, this.gameCanvas.nativeElement.width, this.gameCanvas.nativeElement.height)
+      this.context.fillStyle = "black"
+      this.context.fillRect(position.x, position.y, 20, 20)
     })
 
-    drawBoard(this.gameCanvas);
+    drawBoard(this.gameCanvas)
+
+    this.gameCanvas.nativeElement.addEventListener("click", (event: MouseEvent) => {
+      this.getCursorPosition(this.gameCanvas.nativeElement, event)
+    })
   }
 
   public move(direction: string) {
-    console.log(direction);
-    this.socket.emit("move", direction);
+    console.log(direction)
+    this.socket.emit("move", direction)
   }
+
+  public names = ["up", "down", "left", "right"]
 }
--- client/src/app/display/elements.ts
+++ client/src/app/display/elements.ts
@@ -1,9 +1,9 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef } from '@angular/core'
 
 export interface Element {
-  stroke: string;
-  fill: string;
-  name: string;
+  stroke: string
+  fill: string
+  name: string
 }
 
 export const elements = [
@@ -32,20 +32,22 @@ export const elements = [
   fill: "#330033",
   name: "nether",
 },
-];
+]
 
 export function drawBoard(canvas: ElementRef<HTMLCanvasElement>) {
-  let ctx = canvas.nativeElement.getContext("2d")!;
+  let ctx = canvas.nativeElement.getContext("2d")!
   for(let i=0; i<elements.length; i++) {
-    let element = elements[i];
-    ctx.beginPath();
-    let x = canvas.nativeElement.width/2+150*Math.cos(-Math.PI/2 + i*2*Math.PI/elements.length);
-    let y = canvas.nativeElement.height/2+150*Math.sin(-Math.PI/2 + i*2*Math.PI/elements.length);
-    console.log(`${x}, ${y}`);
-    ctx.arc(x,y,75,0,2*Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = element.fill;
-    ctx.fill();
-    console.log(i);
+    let element = elements[i]
+    ctx.beginPath()
+    let x = canvas.nativeElement.width/2+150*Math.cos(-Math.PI/2 + i*2*Math.PI/elements.length)
+    let y = canvas.nativeElement.height/2+150*Math.sin(-Math.PI/2 + i*2*Math.PI/elements.length)
+    console.log(`${x}, ${y}`)
+    ctx.arc(x,y,75,0,2*Math.PI)
+    ctx.strokeStyle = element.stroke
+    ctx.lineWidth = 6
+    ctx.stroke()
+    ctx.fillStyle = element.fill
+    ctx.fill()
+    console.log(i)
   }
 }
