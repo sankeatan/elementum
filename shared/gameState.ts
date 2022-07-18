696ade0c0adfd408ab4b29bbc280f7b00ad3da26
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-07-18 12:18:24 -0400

    added a game variable with player1 and player2 booleans that can be edited

--- shared/gameState.ts
+++ shared/gameState.ts
@@ -10,3 +10,20 @@ export interface GameState {
   player1: Player;
   player2: Player;
 }
+
+export const game: GameState = {
+  player1: {
+    fire: false,
+    water: false,
+    earth: false,
+    electricity: false,
+    nether: false,
+  },
+  player2: {
+    fire: false,
+    water: false,
+    earth: false,
+    electricity: false,
+    nether: false,
+  }
+}
