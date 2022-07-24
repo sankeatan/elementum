be4a2354d46884771899e18929ebce68bcbadafe
David Ferguson <deejai@live.com>
2022-07-23 23:46:48 -0600

    added script for uploading build files to deploy directory in server. the final step to deploy is to copy /deploy to /live on the server which requires root access. this will be done manually

---
+++ pre-deploy.bat
@@ -0,0 +1,23 @@
+@echo off
+set /p username=Enter remote username: 
+set /p path_to_key=Enter path to private key: 
+set /p passphrase=Enter private key passphrase: 
+"C:\Program Files (x86)\WinSCP\WinSCP.com" ^
+  /log="C:\logs\WinSCP.log" /ini=nul ^
+  /command ^
+    "open sftp://%username%@hogbod.dev/ -hostkey=""ssh-ed25519 255 +o5gMbuLVHUJ6UbMVRJgpgQd+anYwUERHLyFIVt1U5E"" -privatekey=""%path_to_key%"" -passphrase=""%passphrase%""" ^
+    "put C:\Repositories\elementum\build /var/www/hogbod.dev/deploy/elementum/" ^
+    "exit"
+
+set username=
+set path_to_key=
+set passphrase=
+
+set WINSCP_RESULT=%ERRORLEVEL%
+if %WINSCP_RESULT% equ 0 (
+  echo Success
+) else (
+  echo Error
+)
+
+exit /b %WINSCP_RESULT%
